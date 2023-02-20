8346ebff960155422d2eb873df6d4248cdeae85c
Sebastian Zapata <sebastian.zapata@geniussports.com>
2023-02-20 13:23:10 -0500

    funcionando endpoint para obtener un punto especifico

---
+++ pkg/handlers/GetPoint.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/tutorials/go/crud/pkg/models"
+)
+
+func (h handler) GetPoint(w http.ResponseWriter, r *http.Request) {
+	// Read dynamic id parameter
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	// Find User points
+	var user models.User
+
+	if result := h.DB.First(&user, id); result.Error != nil {
+		fmt.Println(result.Error)
+	}
+	pointsMap := make(map[int]int)
+	pointsMap[user.Id] = user.Points
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(pointsMap)
+}
